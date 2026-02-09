c7c467563a0c84d437d35742ff2fad01d2ddb5f6
Makimm137 <1923678930@qq.com>
2026-02-09 23:09:24 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,9 +2,9 @@
 export enum ItemStatus {
   OWNED = '已入手',
   TRANSIT = '在途',
-  WISHLIST = '愿望单',
   RESERVED = '预定中',
-  SOLD = '卖出'
+  SOLD = '卖出',
+  WISHLIST = '愿望单',
 }
 
 export enum PaymentStatus {
@@ -22,12 +22,12 @@ export enum SourceType {
 }
 
 export enum ItemCategory {
+  CD = 'CD',
   BADGE = '吧唧',
   PLUSH = '毛绒',
-  FIGURE = '手办',
-  CARD = '卡片',
+  FIGURE = '立牌',
+  CARD = '纸片',
   ARTBOOK = '画集',
-  CD = 'CD',
   OTHER = '其他'
 }
 
